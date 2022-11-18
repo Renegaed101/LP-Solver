c811db2edfef9f65db985aef8a6f836cc1e2f4ad
Mahd Shahzad <mahd123@hotmail.co.uk>
2022-11-17 19:09:49 -0800

    Refactored readInput and tokenize functions (dictionary.cpp)

--- dictionary.cpp
+++ dictionary.cpp
@@ -8,36 +8,27 @@ class dictionary {
 
         //Helper function for readInput that takes a string splits words and stores 
         //in a vector
-        std::vector<std::string> tokenize (const std::string& line) {
-            std::vector<std::string> result {};
+        void tokenize (const std::string& line) {
             std::string word;
             for (const auto& c : line) {
                 if (c == ' ' || c == '\t') {
                     if (word != "") 
-                        result.push_back(word);
+                        dic.back().push_back(std::stod(word));
                     word = "";
                 }
                 else {
                     word = word + c;
                 }
             }
-            return result;
         }
 
         //Reads in input from stdin and stores in dictionary form
         void readInput () {  
-
-            std::vector<std::string> strings {};
             
             std::string line;
-            int i = 0;
             while (std::getline(std::cin,line)) {
                 dic.push_back(std::vector<fraction>{});
-                strings = tokenize(line + " ");                    
-                for (const auto& word : strings) {
-                    dic.at(i).push_back(std::stod(word));       
-                }
-                i++;
+                tokenize(line + " ");                    
             }
             dic.at(0).insert(dic.at(0).begin(),fraction{0});
         }
@@ -47,6 +38,8 @@ class dictionary {
             readInput();
         } 
 
+        
+        //Prints out contents of dictionary
         void print() {
             for (const auto& i: dic) {
                 for (const auto& j: i) {
